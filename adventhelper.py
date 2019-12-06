fc4d9275fc063b673a1b549ac2b5c0848d91d278
Adam Vincent <automatedempire@users.noreply.github.com>
2019-12-05 19:40:24 -0500

    Adding helper module
    Creating adventhelper to handle downloading input so I don't have to copy that code to each new day. Adding the pycache to gitignore. Creating a template for new days based on the helper module.

---
+++ adventhelper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import requests
+import os
+
+def download_input(day):
+    dayinput=f'd{day}.input'
+
+    if not(os.path.exists(dayinput) and os.path.isfile(dayinput)):
+        print("Input doesn't exist yet. Downloading...")
+        url=f'https://adventofcode.com/2019/day/{day}/input'
+        #Must grab this from a logged in session.
+        cookiefile='session.cookie'
+        with open(cookiefile) as c:
+            cookie=c.readline().strip()
+            cookies=dict(session=cookie)
+        response = requests.get(url, cookies=cookies)
+        with open(dayinput,'w') as d_in:
+            d_in.write(response.text)
+
+    return dayinput
