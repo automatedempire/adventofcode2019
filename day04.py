bb1bb4cdd01948d8f6b8d6c5b16a00527dc12732
Adam Vincent <automatedempire@users.noreply.github.com>
2019-12-07 22:49:33 -0500

    Code for star eight
    Added another regex pattern for the new criteria. This one uses a negative look-behind to not match when 3 of the same digit are grouped together.
    This also precludes any sequence longer than 3.

--- day04.py
+++ day04.py
@@ -12,16 +12,23 @@ with open(dayinput) as d_in:
 
 #two of the same digit together
 doubles = re.compile(r"(.)\1")
+justdoubles = re.compile(r"(.)\1(?<!\1{3})(?!\1)")
 
 possibilities = []
+betterpossibilities = []
 
 for x in range(minpass,maxpass):
     numstring = str(x)
     hasdouble = doubles.search(numstring)
+    justdouble = justdoubles.search(numstring)
     s = "".join(sorted(numstring))
     inOrder = numstring == s
 
     if hasdouble and inOrder:
         possibilities.append(x)
 
+    if justdouble and inOrder:
+        betterpossibilities.append(x)
+
 print(f"Total possible passwords: {len(possibilities)}")
+print(f"Total better possible passwords: {len(betterpossibilities)}")
